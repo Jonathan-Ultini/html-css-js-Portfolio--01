b96e908f8f8c32b18556c84391eef80e4c44b361
Jonathan-Ultini <jonathanultini@gmail.com>
2024-08-01 20:14:49 +0200

    comments

--- js/script.js
+++ js/script.js
@@ -1,26 +1,34 @@
 function scrollToElement(elementSelector, instance = 0) {
-  // Select all elements that match the given selector
+  // Seleziona tutti gli elementi che corrispondono al selettore specificato
   const elements = document.querySelectorAll(elementSelector);
-  // Check if there are elements matching the selector and if the requested instance exists
+  // Verifica se ci sono elementi che corrispondono al selettore e se esiste l'istanza richiesta
   if (elements.length > instance) {
-    // Scroll to the specified instance of the element
+    // Scorri fino all'istanza specificata dell'elemento
     elements[instance].scrollIntoView({ behavior: 'smooth' });
   }
 }
 
+// Seleziona il link con l'ID "link1"
 const link1 = document.getElementById("link1");
+// Seleziona il link con l'ID "link2"
 const link2 = document.getElementById("link2");
+// Seleziona il link con l'ID "link3"
 const link3 = document.getElementById("link3");
 
+// Aggiungi un listener di eventi per il click sul link1
 link1.addEventListener('click', () => {
+  // Chiama la funzione scrollToElement per scorrere fino al primo elemento con la classe "header"
   scrollToElement('.header');
 });
 
+// Aggiungi un listener di eventi per il click sul link2
 link2.addEventListener('click', () => {
-  // Scroll to the second element with "header" class
+  // Scorri fino al secondo elemento con la classe "header"
   scrollToElement('.header', 1);
 });
 
+// Aggiungi un listener di eventi per il click sul link3
 link3.addEventListener('click', () => {
+  // Chiama la funzione scrollToElement per scorrere fino al primo elemento con la classe "column"
   scrollToElement('.column');
 });
